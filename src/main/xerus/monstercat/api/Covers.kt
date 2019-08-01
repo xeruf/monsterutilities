9ebc6eccf9167a194dd295e1019631ba714ca4eb
Daniel Thirion <defvs.daniel@gmail.com>
2019-08-01 19:00:38 +0200

    style(covers): revert indent to correct style

--- src/main/xerus/monstercat/api/Covers.kt
+++ src/main/xerus/monstercat/api/Covers.kt
@@ -19,13 +19,11 @@ object Covers {
 		return file.resolve("${newFile.nameWithoutExtension}-${size}x$size.${newFile.extension}")
 	}
 	
-	private fun createImage(content: InputStream, size: Number) =
-			Image(content, size.toDouble(), size.toDouble(), false, false)
+	private fun createImage(content: InputStream, size: Number) = Image(content, size.toDouble(), size.toDouble(), false, false)
 	
 	/** Returns an Image of the cover in the requested size using caching.
 	 * @param size the size of the Image - the underlying image data will always be 64x64, thus this is the default. */
-	fun getThumbnailImage(coverUrl: String, size: Number = 64, invalidate: Boolean = false): Image =
-		getCover(coverUrl, 64,  invalidate).use { createImage(it, size) }
+	fun getThumbnailImage(coverUrl: String, size: Number = 64, invalidate: Boolean = false): Image = getCover(coverUrl, 64,  invalidate).use { createImage(it, size) }
 
 	/** @return an [InputStream] of the downloaded image file
 	 * @param coverUrl the URL for fetching the cover
@@ -46,18 +44,15 @@ object Covers {
 	
 	/** Returns a larger Image of the cover in the requested size using caching.
 	 * @param size the size of the Image - the image file will always be 2048x2048 */
-	fun getCoverImage(coverUrl: String, size: Int = 2048, invalidate: Boolean = false): Image =
-			getCover(coverUrl, 2048, invalidate).use { createImage(it, size) }
+	fun getCoverImage(coverUrl: String, size: Int = 2048, invalidate: Boolean = false): Image = getCover(coverUrl, 2048, invalidate).use { createImage(it, size) }
 	
 	/** Fetches the given [coverUrl] with an [APIConnection] in the requested [size].
 	 * @param coverUrl the base url to fetch the cover
 	 * @param size the size of the cover to be fetched from the api, with all powers of 2 being available.
 	 *             By default null, which results in the biggest size possible, usually between 2k and 8k. */
-	fun fetchCover(coverUrl: String, size: Int? = null): HttpEntity =
-		APIConnection.executeRequest(HttpGet(getCoverUrl(coverUrl, size))).entity
+	fun fetchCover(coverUrl: String, size: Int? = null): HttpEntity = APIConnection.executeRequest(HttpGet(getCoverUrl(coverUrl, size))).entity
 	
 	/** Attaches a parameter to the [coverUrl] for the requested [size] */
-	private fun getCoverUrl(coverUrl: String, size: Int? = null) =
-		URI(coverUrl).toASCIIString() + size?.let { "?image_width=$it" }.orEmpty()
+	private fun getCoverUrl(coverUrl: String, size: Int? = null) = URI(coverUrl).toASCIIString() + size?.let { "?image_width=$it" }.orEmpty()
 	
 }
